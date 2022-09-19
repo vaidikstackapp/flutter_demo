eefbb9e6685e046f5a6ac0e0effb8efbaae80be9
vaidikstackapp <113659859+vaidikstackapp@users.noreply.github.com>
2022-09-19 15:17:03 +0530

    Update user_service1.dart

--- lib/service/user_service1.dart
+++ lib/service/user_service1.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 
 import '../model/user_model.dart';
 
@@ -6,11 +8,13 @@ class UserService1 {
   final CollectionReference collectionReference =
       FirebaseFirestore.instance.collection('user1');
 
-  Future<void> setUser(UserModel userModel) async {
+  Future<void> setUser(UserModel userModel, BuildContext context) async {
     try {
       await collectionReference.doc(userModel.uid).set(userModel.toJson());
     } on FirebaseException catch (e) {
-      print("Catch exception on setUser : $e");
+      if (kDebugMode) {
+        print("Catch exception on setUser : $e");
+      }
     }
   }
 }
