791631e2520271682d9773a0ccc9f6c4ac2518bc
vaidikstackapp <113659859+vaidikstackapp@users.noreply.github.com>
2022-09-21 10:51:57 +0530

    Update ui

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -28,158 +28,201 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       body: Form(
         key: _formKey,
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  textInputAction: TextInputAction.next,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return StringConstants.isEmailEmpty;
-                    } else if (!StringConstants.emailPatten.hasMatch(value)) {
-                      return StringConstants.isEmailNotMatch;
-                    }
-                    return null;
-                  },
-                  controller: tEmail,
-                  cursorColor: ColorConstants.commonColor,
-                  decoration: InputDecoration(
-                    label: Text(
-                      StringConstants.email,
-                      style: TextStyle(color: ColorConstants.commonColor),
-                    ),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: ColorConstants.commonColor,
-                      ),
-                    ),
-                    enabledBorder: outLineInputBorder(),
-                    disabledBorder: outLineInputBorder(),
-                    focusedBorder: outLineInputBorder(),
-                    errorBorder:
-                        outLineInputBorder(color: ColorConstants.errorColor),
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+          child: Center(
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                const Text(
+                  textAlign: TextAlign.center,
+                  "Log In Now",
+                  style: TextStyle(color: Colors.black, fontSize: 30),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  child: const Text(
+                    textAlign: TextAlign.center,
+                    "please login to continue using our app",
+                    style: TextStyle(color: Colors.black54),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  textInputAction: TextInputAction.next,
-                  obscureText: visible,
-                  obscuringCharacter: "*",
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return StringConstants.isPasswordEmpty;
-                    } else if (!StringConstants.passPatten.hasMatch(value)) {
-                      return StringConstants.isPasswordNotMatch;
-                    }
-                    return null;
-                  },
-                  controller: tPassword,
-                  decoration: InputDecoration(
-                    suffixIcon: IconButton(
-                        onPressed: () {
-                          visible = !visible;
-                          setState(() {});
-                        },
-                        icon: (visible)
-                            ? Icon(
-                                Icons.visibility_off,
-                                color: ColorConstants.commonColor,
-                              )
-                            : Icon(Icons.visibility,
-                                color: ColorConstants.commonColor)),
-                    label: Text(
-                      StringConstants.password,
-                      style: TextStyle(
-                        color: ColorConstants.commonColor,
-                      ),
-                    ),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: ColorConstants.commonColor,
-                      ),
-                    ),
-                    enabledBorder: outLineInputBorder(),
-                    disabledBorder: outLineInputBorder(),
-                    focusedBorder: outLineInputBorder(),
-                    errorBorder:
-                        outLineInputBorder(color: ColorConstants.errorColor),
-                  ),
-                  cursorColor: ColorConstants.commonColor,
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("User Type :-"),
-                  Radio(
-                    value: false,
-                    groupValue: isAdmin,
-                    onChanged: (value) {
-                      isAdmin = !isAdmin;
-                      //print("Normal : $isAdmin");
-                      setState(() {});
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    textInputAction: TextInputAction.next,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return StringConstants.isEmailEmpty;
+                      } else if (!StringConstants.emailPatten.hasMatch(value)) {
+                        return StringConstants.isEmailNotMatch;
+                      }
+                      return null;
                     },
+                    controller: tEmail,
+                    cursorColor: ColorConstants.commonColor,
+                    decoration: InputDecoration(
+                      label: Text(
+                        StringConstants.email,
+                        style: TextStyle(color: ColorConstants.commonColor),
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: ColorConstants.commonColor,
+                        ),
+                      ),
+                      enabledBorder: outLineInputBorder(),
+                      disabledBorder: outLineInputBorder(),
+                      focusedBorder: outLineInputBorder(),
+                      errorBorder:
+                          outLineInputBorder(color: ColorConstants.errorColor),
+                    ),
                   ),
-                  const Text('Normal'),
-                  Radio(
-                    value: true,
-                    groupValue: isAdmin,
-                    onChanged: (value) {
-                      isAdmin = !isAdmin;
-                      //  print("admin : $isAdmin");
-                      setState(() {});
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    textInputAction: TextInputAction.next,
+                    obscureText: visible,
+                    obscuringCharacter: "*",
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return StringConstants.isPasswordEmpty;
+                      } else if (!StringConstants.passPatten.hasMatch(value)) {
+                        return StringConstants.isPasswordNotMatch;
+                      }
+                      return null;
                     },
+                    controller: tPassword,
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            visible = !visible;
+                            setState(() {});
+                          },
+                          icon: (visible)
+                              ? Icon(
+                                  Icons.visibility_off,
+                                  color: ColorConstants.commonColor,
+                                )
+                              : Icon(Icons.visibility,
+                                  color: ColorConstants.commonColor)),
+                      label: Text(
+                        StringConstants.password,
+                        style: TextStyle(
+                          color: ColorConstants.commonColor,
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: ColorConstants.commonColor,
+                        ),
+                      ),
+                      enabledBorder: outLineInputBorder(),
+                      disabledBorder: outLineInputBorder(),
+                      focusedBorder: outLineInputBorder(),
+                      errorBorder:
+                          outLineInputBorder(color: ColorConstants.errorColor),
+                    ),
+                    cursorColor: ColorConstants.commonColor,
                   ),
-                  const Text('Admin'),
-                ],
-              ),
-              ElevatedButton(
-                  onPressed: () async {
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Type : "),
+                    Radio(
+                      value: false,
+                      groupValue: isAdmin,
+                      onChanged: (value) {
+                        isAdmin = !isAdmin;
+                        //print("Normal : $isAdmin");
+                        setState(() {});
+                      },
+                    ),
+                    const Text('User'),
+                    Radio(
+                      value: true,
+                      groupValue: isAdmin,
+                      onChanged: (value) {
+                        isAdmin = !isAdmin;
+                        //  print("admin : $isAdmin");
+                        setState(() {});
+                      },
+                    ),
+                    const Text('Admin'),
+                  ],
+                ),
+                GestureDetector(
+                  onTap: () {
                     FocusManager.instance.primaryFocus?.unfocus();
                     if (_formKey.currentState!.validate()) {
                       AuthService().authentication(
                           tEmail, tPassword, isAdmin, widget.tabController);
                     }
                   },
-                  child: Text(StringConstants.signIn)),
-              InkWell(
-                onTap: () async {
-                  AuthService().signInWithGoogle(widget.tabController, isAdmin);
-                },
-                child: Container(
-                  height: 40,
-                  width: 160,
-                  decoration: BoxDecoration(
-                      color: ColorConstants.commonColor,
-                      borderRadius: BorderRadius.circular(5),
-                      boxShadow: [
-                        BoxShadow(
-                            color: ColorConstants.borderColor,
-                            offset: const Offset(0, 5),
-                            blurRadius: 5),
-                      ]),
-                  padding: const EdgeInsets.symmetric(horizontal: 5),
-                  alignment: Alignment.center,
-                  child: Row(
-                    children: [
-                      Image.asset(
-                        "images/google.png",
-                        height: 20,
-                        width: 20,
-                        fit: BoxFit.cover,
-                      ),
-                      Text(StringConstants.signInWithGoogle,
-                          style: TextStyle(color: ColorConstants.textColor)),
-                    ],
+                  child: Container(
+                    width: 160,
+                    height: 40,
+                    decoration: BoxDecoration(
+                        color: ColorConstants.commonColor,
+                        borderRadius: BorderRadius.circular(5),
+                        boxShadow: [
+                          BoxShadow(
+                              color: ColorConstants.borderColor,
+                              offset: const Offset(0, 5),
+                              blurRadius: 5),
+                        ]),
+                    alignment: Alignment.center,
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 50, vertical: 10)
+                            .copyWith(top: 30),
+                    child: Text(StringConstants.signIn,
+                        style: TextStyle(color: ColorConstants.textColor)),
                   ),
                 ),
-              ),
-            ],
+                GestureDetector(
+                  onTap: () async {
+                    AuthService()
+                        .signInWithGoogle(widget.tabController, isAdmin);
+                  },
+                  child: Container(
+                    height: 40,
+                    width: 160,
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+                    decoration: BoxDecoration(
+                        color: ColorConstants.commonColor,
+                        borderRadius: BorderRadius.circular(5),
+                        boxShadow: [
+                          BoxShadow(
+                              color: ColorConstants.borderColor,
+                              offset: const Offset(0, 5),
+                              blurRadius: 5),
+                        ]),
+                    padding: const EdgeInsets.symmetric(horizontal: 5),
+                    alignment: Alignment.center,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          "images/google.png",
+                          height: 20,
+                          width: 20,
+                          fit: BoxFit.cover,
+                        ),
+                        Text(StringConstants.signInWithGoogle,
+                            style: TextStyle(color: ColorConstants.textColor)),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
